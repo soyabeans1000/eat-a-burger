beadd3d344efac58d8649ad68393c150c7b13722
Sarika <sarika@ngoyal.com>
2019-05-21 15:38:52 -0700

    Removed Database

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ app.set('view engine', '.hbs')
 require('./routes')(app)
 
 
-console.log('server')
-require('./config').connect(_ => { app.listen(3000) })
+console.log('inside server')
+require('./config').connect(_ => { app.listen(3000)
+console.log('connection suces') })
