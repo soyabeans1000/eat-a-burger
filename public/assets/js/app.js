e304f4818ccf177e375a2f04e17a42a64d2cebdd
Sarika <sarika@ngoyal.com>
2019-05-20 17:07:20 -0700

    Front End Setup

---
+++ public/assets/js/app.js
@@ -0,0 +1,16 @@
+const { fetch, alert } = window
+
+document.addEventListener('click', e => {
+    e.preventDefault()
+    //if trash button clicked
+    if (e.target.className === 'fas fa-concierge-bell') {
+    //   console.log(e.target.getAttribute('train_id'))
+    console.log('Clicked')
+    }
+    
+}
+    )
+
+
+
+
--- routes/burgerRoutes.js
+++ routes/burgerRoutes.js
@@ -22,9 +22,23 @@ module.exports = app => {
     db.query('SELECT * FROM burger_orders', (e, orders) => {
       if (e) throw e
       //res.json(orders)
+
       res.render('index', {orders} )
     }) 
     })
+
+    
+
+
+      app.post('/burgers/:id', (req, res) => {
+        // db.query('UPDATE users SET ? WHERE ?', [req.body, { id: req.params.id }], e => {
+        //   if (e) throw e
+        //   res.send('ok')
+        // })
+
+        console.log("Updating now")
+
+      })
 
 
 
