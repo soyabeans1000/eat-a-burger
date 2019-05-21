0f8e0ea198b67ebeb62110cb877191e4ed6110e6
Sarika <sarika@ngoyal.com>
2019-05-21 16:44:25 -0700

    changed connection file

--- config/index.js
+++ config/index.js
@@ -3,12 +3,12 @@ var mysql = require('mysql')
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)({
-  host: 'sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  port: 3306,
-  user: 'kk8btmt9ysim52j9',
-  password: 'g705fr1c1woss4bd',
-  database: 'a8n15k71l54nqsi3'
+  connection = mysql.createConnection({
+    host: 'sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'g705fr1c1woss4bd',
+    password: 'g705fr1c1woss4bd',
+    database: 'a8n15k71l54nqsi3'
   })
 }
 
