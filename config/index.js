19556821c1f6d16501995fd9e7fba3a2601b0b6c
Sarika <sarika@ngoyal.com>
2019-05-21 15:01:18 -0700

    Removed Database

--- config/index.js
+++ config/index.js
@@ -2,12 +2,7 @@ var mysql = require('mysql')
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-}
-
-else 
-{
+// /
 
   connection = mysql.createConnection({
     host: 'localhost',
@@ -17,7 +12,7 @@ else
     database: 'burger_db'
       })
 
-}
+// }
 
 
 //connection.connect()
